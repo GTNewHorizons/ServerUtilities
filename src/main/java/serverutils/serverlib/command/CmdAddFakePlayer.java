97a0bc857c303e5e70a2b698904741ec958fa046
Ethryan <3237986+Ethryan@users.noreply.github.com>
2023-09-24 23:48:52 +0200

    command main

--- src/main/java/serverutils/serverlib/command/CmdAddFakePlayer.java
+++ src/main/java/serverutils/serverlib/command/CmdAddFakePlayer.java
@@ -1,46 +1,38 @@
 package serverutils.serverlib.command;
 
+import java.util.UUID;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+
 import serverutils.serverlib.ServerLib;
 import serverutils.serverlib.lib.command.CmdBase;
 import serverutils.serverlib.lib.data.ForgePlayer;
 import serverutils.serverlib.lib.data.Universe;
 import serverutils.serverlib.lib.util.StringUtils;
-import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommandSender;
-import net.minecraft.server.MinecraftServer;
 
-import java.util.UUID;
+public class CmdAddFakePlayer extends CmdBase {
 
-/**
- * @author LatvianModder
- */
-public class CmdAddFakePlayer extends CmdBase
-{
-	public CmdAddFakePlayer()
-	{
+	public CmdAddFakePlayer() {
 		super("add_fake_player", Level.OP);
 	}
 
 	@Override
-	public boolean isUsernameIndex(String[] args, int index)
-	{
+	public boolean isUsernameIndex(String[] args, int index) {
 		return index == 0;
 	}
 
 	@Override
-	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-	{
+	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
 		checkArgs(sender, args, 2);
 
 		UUID id = StringUtils.fromString(args[0]);
 
-		if (id == null)
-		{
+		if (id == null) {
 			throw ServerLib.error(sender, "serverlib.lang.add_fake_player.invalid_uuid");
 		}
 
-		if (Universe.get().getPlayer(id) != null || Universe.get().getPlayer(args[1]) != null)
-		{
+		if (Universe.get().getPlayer(id) != null || Universe.get().getPlayer(args[1]) != null) {
 			throw ServerLib.error(sender, "serverlib.lang.add_fake_player.player_exists");
 		}
 
--- src/main/java/serverutils/serverlib/command/CmdMySettings.java
+++ src/main/java/serverutils/serverlib/command/CmdMySettings.java
@@ -1,28 +1,26 @@
 package serverutils.serverlib.command;
 
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+
 import serverutils.serverlib.lib.command.CmdEditConfigBase;
 import serverutils.serverlib.lib.command.CommandUtils;
 import serverutils.serverlib.lib.config.ConfigGroup;
 import serverutils.serverlib.lib.config.IConfigCallback;
-import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommandSender;
 
-public class CmdMySettings extends CmdEditConfigBase
-{
-	public CmdMySettings()
-	{
+public class CmdMySettings extends CmdEditConfigBase {
+
+	public CmdMySettings() {
 		super("my_settings", Level.ALL);
 	}
 
 	@Override
-	public ConfigGroup getGroup(ICommandSender sender) throws CommandException
-	{
+	public ConfigGroup getGroup(ICommandSender sender) throws CommandException {
 		return CommandUtils.getForgePlayer(sender).getSettings();
 	}
 
 	@Override
-	public IConfigCallback getCallback(ICommandSender sender) throws CommandException
-	{
+	public IConfigCallback getCallback(ICommandSender sender) throws CommandException {
 		return CommandUtils.getForgePlayer(sender);
 	}
 }
