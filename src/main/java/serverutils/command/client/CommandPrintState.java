4cd29efc46cedf0eeebb674aeb58d631d70e9806
Lyfts <127234178+Lyfts@users.noreply.github.com>
2024-01-03 10:24:09 +0100

    Fix /print_block_state returning an error since player is EntityClientPlayerMP

--- src/main/java/serverutils/command/client/CommandPrintState.java
+++ src/main/java/serverutils/command/client/CommandPrintState.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.event.ClickEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
@@ -19,6 +20,9 @@ public class CommandPrintState extends CmdBase {
 
     @Override
     public void processCommand(ICommandSender sender, final String[] args) throws CommandException {
+        if (!(sender instanceof EntityPlayer player)) {
+            return;
+        }
         MovingObjectPosition ray = Minecraft.getMinecraft().objectMouseOver;
         if (ray.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK) {
             return;
@@ -27,13 +31,9 @@ public class CommandPrintState extends CmdBase {
         Block block = sender.getEntityWorld().getBlock(ray.blockX, ray.blockY, ray.blockZ);
 
         IChatComponent component = new ChatComponentText(
-                block.getPickBlock(
-                        ray,
-                        sender.getEntityWorld(),
-                        ray.blockX,
-                        ray.blockY,
-                        ray.blockZ,
-                        getCommandSenderAsPlayer(sender)).getDisplayName() + " :: " + block.getUnlocalizedName());
+                block.getPickBlock(ray, sender.getEntityWorld(), ray.blockX, ray.blockY, ray.blockZ, player)
+                        .getDisplayName() + " :: "
+                        + block.getUnlocalizedName());
         component.getChatStyle()
                 .setChatClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, block.getUnlocalizedName()));
         sender.addChatMessage(component);
