8c6e21b8166111515c523b8c6d661d023008ca84
Lyft <127234178+Lyfts@users.noreply.github.com>
2024-12-10 15:55:57 +0100

    Fix display name not syncing again (#150)
    Signed-off-by: Lyfts <127234178+Lyfts@users.noreply.github.com>

--- src/main/java/serverutils/net/MessageRankModify.java
+++ src/main/java/serverutils/net/MessageRankModify.java
@@ -2,25 +2,18 @@ package serverutils.net;
 
 import static serverutils.ServerUtilitiesPermissions.RANK_EDIT;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentText;
 
-import serverutils.ServerUtilitiesConfig;
-import serverutils.ServerUtilitiesPermissions;
 import serverutils.client.gui.ranks.RankInst;
 import serverutils.lib.config.ConfigValueInstance;
-import serverutils.lib.data.ForgePlayer;
-import serverutils.lib.data.Universe;
 import serverutils.lib.io.DataIn;
 import serverutils.lib.io.DataOut;
 import serverutils.lib.net.MessageToServer;
 import serverutils.lib.net.NetworkWrapper;
 import serverutils.lib.util.permission.PermissionAPI;
-import serverutils.ranks.PlayerRank;
 import serverutils.ranks.Rank;
 import serverutils.ranks.Ranks;
 
@@ -62,40 +55,22 @@ public class MessageRankModify extends MessageToServer {
             return;
         }
 
-        boolean updateNames = false;
         boolean shouldSave = false;
         for (ConfigValueInstance value : inst.group.getValues()) {
             Rank.Entry entry = rank.setPermission(value.getId(), value.getValue());
             if (entry == null) continue;
-            if (entry.node.equals(ServerUtilitiesPermissions.CHAT_NAME_FORMAT)) {
-                updateNames = true;
-            }
             shouldSave = true;
         }
 
         for (String removed : removedEntries) {
             Rank.Entry entry = rank.setPermission(removed, "");
             if (entry == null) continue;
-            if (entry.node.equals(ServerUtilitiesPermissions.CHAT_NAME_FORMAT)) {
-                updateNames = true;
-            }
             shouldSave = true;
 
         }
 
         if (shouldSave) {
             Ranks.INSTANCE.save();
-        }
-
-        if (ServerUtilitiesConfig.chat.replace_tab_names && updateNames) {
-            List<ForgePlayer> toUpdate = new ArrayList<>();
-            for (PlayerRank playerRank : Ranks.INSTANCE.playerRanks.values()) {
-                if (!playerRank.getParents().contains(rank)) continue;
-                ForgePlayer fp = Universe.get().getPlayer(playerRank.profile);
-                if (fp == null || !fp.isOnline()) continue;
-                toUpdate.add(fp);
-            }
-            new MessageUpdateTabName(toUpdate).sendToAll();
         }
     }
 }
