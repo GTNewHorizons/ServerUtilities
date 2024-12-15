5e78c08488773d303e5e81b00ff5fce9170956ad
Lyfts <127234178+Lyfts@users.noreply.github.com>
2024-12-15 12:20:26 +0100

    fix permissions not working for subcommands
    Signed-off-by: Lyfts <127234178+Lyfts@users.noreply.github.com>

--- src/mixins/java/serverutils/mixins/early/minecraft/MixinICommand.java
+++ src/mixins/java/serverutils/mixins/early/minecraft/MixinICommand.java
@@ -1,18 +1,25 @@
 package serverutils.mixins.early.minecraft;
 
 import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import cpw.mods.fml.common.eventhandler.Event;
 import serverutils.ranks.ICommandWithPermission;
+import serverutils.ranks.Ranks;
 
 @Mixin(ICommand.class)
 public interface MixinICommand extends ICommandWithPermission {
 
     @Shadow
     String getCommandName();
+
+    @Shadow
+    boolean canCommandSenderUseCommand(ICommandSender sender);
 
     @Override
     default String serverutilities$getPermissionNode() {
@@ -32,5 +39,15 @@ public interface MixinICommand extends ICommandWithPermission {
     @Override
     default void serverutilities$setModName(@NotNull String modName) {
         commandOwners.put(getCommandName(), modName);
+    }
+
+    @Override
+    default boolean serverutilities$hasPermission(@NotNull EntityPlayerMP player) {
+        Event.Result result = Ranks.INSTANCE.getPermissionResult(player, serverutilities$getPermissionNode(), true);
+        if (result == Event.Result.DEFAULT) {
+            return canCommandSenderUseCommand(player);
+        }
+
+        return result == Event.Result.ALLOW;
     }
 }
