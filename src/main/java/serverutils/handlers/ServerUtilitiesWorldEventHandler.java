a8b50aa474e0da0fcbdb297f749bba6a80021b07
LazyFlesh <40587985+LazyFlesh@users.noreply.github.com>
2026-07-08 16:05:19 -0700

    Revert "lang; event may not be cancelable"
    This reverts commit e455fbc711fcc1bac7e40499f266c40cf70249a7.

--- src/main/java/serverutils/handlers/ServerUtilitiesWorldEventHandler.java
+++ src/main/java/serverutils/handlers/ServerUtilitiesWorldEventHandler.java
@@ -21,7 +21,6 @@ import net.minecraftforge.event.world.WorldEvent;
 
 import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
 
-import cpw.mods.fml.common.eventhandler.Event;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import serverutils.ServerUtilities;
 import serverutils.ServerUtilitiesConfig;
@@ -40,9 +39,9 @@ public class ServerUtilitiesWorldEventHandler {
 
     @SubscribeEvent
     public static void onMobSpawned(LivingSpawnEvent event) {
-        if (!event.world.isRemote && !isEntityAllowed(event)) {
+        if (!event.world.isRemote && !isEntityAllowed(event.entity)) {
             event.entity.setDead();
-            event.setResult(Event.Result.DENY);
+            event.setCanceled(true);
         }
     }
 
